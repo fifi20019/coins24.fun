8b62a7737cfa256dc8ebbc8286dbb60d97873b94
fifi20019 <88151656+fifi20019@users.noreply.github.com>
2021-07-29 16:09:28 +0100

    Add files via upload

---
+++ parts/resource-one.php
@@ -0,0 +1,66 @@
+<div class="row">	
+	<div class="col-md-3">	
+		<div class="connected-player-wrapper">	
+			<div class="connected-player-content animated bounceIn">
+				<div class="label animated bounceIn">Player Connected</div>
+				<div class="connected-player-inner">	
+					<div class="connected-player-item animated bounceIn animation-delay-200">
+						<div class="connected-label">Username:</div>
+						<div class="connected-player-value connected-username-value"></div>
+					</div>
+					<div class="connected-player-item connected-player-item-2 animated bounceIn animation-delay-400">
+						<div class="connected-label">Device:</div>
+						<div class="connected-player-value connected-device-value"></div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<div class="col-md-9">	
+		<div class="resource-select-wrapper">	
+			<div class="resource-one-wrapper">
+				<div class="animated bounceIn animation-delay-600">
+					<div class="label">Amount of CoD Points</div>
+				</div>
+				<div class="row small-margin">	
+					<div class="col-6 small-padding">	
+						<div class="animated bounceIn animation-delay-800">
+							<div class="resource-select-item resource-select-item-one resource-select-item-1">
+								<img src="img/r-item-1.png" class="resource-select-icon" />
+								<div class="resource-item-value resource-item-one-value resource-item-value-1"></div>
+								<div class="resource-item-label">CoD Points</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-6 small-padding">	
+						<div class="animated bounceIn animation-delay-1000">
+							<div class="resource-select-item resource-select-item-one resource-select-item-2">
+								<img src="img/r-item-1.png" class="resource-select-icon" />
+								<div class="resource-item-value resource-item-one-value resource-item-value-2"></div>
+								<div class="resource-item-label">CoD Points</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-6 small-padding">	
+						<div class="animated bounceIn animation-delay-1200">
+							<div class="resource-select-item resource-select-item-one resource-select-item-3">
+								<img src="img/r-item-1.png" class="resource-select-icon" />
+								<div class="resource-item-value resource-item-one-value resource-item-value-3"></div>
+								<div class="resource-item-label">CoD Points</div>
+							</div>
+						</div>
+					</div>	
+					<div class="col-6 small-padding">	
+						<div class="animated bounceIn animation-delay-1400">
+							<div class="resource-select-item resource-select-item-one resource-select-item-4">
+								<img src="img/r-item-1.png" class="resource-select-icon" />
+								<div class="resource-item-value resource-item-one-value resource-item-value-4"></div>
+								<div class="resource-item-label">CoD Points</div>
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
